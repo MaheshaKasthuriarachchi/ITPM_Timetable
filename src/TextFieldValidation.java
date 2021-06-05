be83781d3279f8ffef57ac897609a3b552c649c8
CS COMPUTERS <CS COMPUTERS@DESKTOP-2VQE866>
2021-06-05 18:42:35 +0530

    member3

---
+++ src/TextFieldValidation.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author CS COMPUTERS
+ */
+public class TextFieldValidation {
+    
+    public static void main(String[] args){
+        inf2 frame = new inf2();
+        frame.show();
+        
+    }
+    
+    
+}
